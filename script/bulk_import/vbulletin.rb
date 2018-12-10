c67facbb4e7ec2adc2e51cc59be85ccff5d75a77
Arpit Jalan <arpit@techapj.com>
2018-12-10 22:17:44 +0530

    interNACHI import

--- script/bulk_import/vbulletin.rb
+++ script/bulk_import/vbulletin.rb
@@ -7,13 +7,14 @@ class BulkImport::VBulletin < BulkImport::Base
 
   TABLE_PREFIX = "vb_"
   SUSPENDED_TILL ||= Date.new(3000, 1, 1)
+  ATTACHMENT_DIR ||= ENV['ATTACHMENT_DIR'] || '/shared/import/data/attachments'
 
   def initialize
     super
 
-    host     = ENV["DB_HOST"] || "localhost"
+    host     = ENV["DB_HOST"] || "127.0.0.1"
     username = ENV["DB_USERNAME"] || "root"
-    password = ENV["DB_PASSWORD"]
+    password = ENV["DB_PASSWORD"] || ""
     database = ENV["DB_NAME"] || "vbulletin"
     charset  = ENV["DB_CHARSET"] || "utf8"
 
@@ -30,17 +31,26 @@ class BulkImport::VBulletin < BulkImport::Base
 
     @client.query_options.merge!(as: :array, cache_rows: false)
 
-    @has_post_thanks = mysql_query(<<-SQL
-        SELECT `COLUMN_NAME`
-          FROM `INFORMATION_SCHEMA`.`COLUMNS`
-         WHERE `TABLE_SCHEMA`='#{database}'
-           AND `TABLE_NAME`='user'
-           AND `COLUMN_NAME` LIKE 'post_thanks_%'
-    SQL
-    ).to_a.count > 0
+    # @has_post_thanks = mysql_query(<<-SQL
+    #     SELECT `COLUMN_NAME`
+    #       FROM `INFORMATION_SCHEMA`.`COLUMNS`
+    #      WHERE `TABLE_SCHEMA`='#{database}'
+    #        AND `TABLE_NAME`='user'
+    #        AND `COLUMN_NAME` LIKE 'post_thanks_%'
+    # SQL
+    # ).to_a.count > 0
+    @has_post_thanks = false
   end
 
   def execute
+    SiteSetting.automatic_backups_enabled = false
+    SiteSetting.disable_emails = "non-staff"
+    SiteSetting.authorized_extensions = '*'
+    SiteSetting.max_image_size_kb = 102400
+    SiteSetting.max_attachment_size_kb = 102400
+    SiteSetting.clean_up_uploads = false
+    SiteSetting.clean_orphan_uploads_grace_period_hours = 43200
+
     import_groups
     import_users
     import_group_users
@@ -61,6 +71,10 @@ class BulkImport::VBulletin < BulkImport::Base
     import_private_topics
     import_topic_allowed_users
     import_private_posts
+
+    create_permalink_file
+
+    import_attachments
   end
 
   def import_groups
@@ -76,7 +90,7 @@ class BulkImport::VBulletin < BulkImport::Base
     create_groups(groups) do |row|
       {
         imported_id: row[0],
-        name: normalize_text(row[1]),
+        name: normalize_text(row[1])&.dasherize&.downcase,
         bio_raw: normalize_text(row[2]),
         title: normalize_text(row[3]),
       }
@@ -87,9 +101,11 @@ class BulkImport::VBulletin < BulkImport::Base
     puts "Importing users..."
 
     users = mysql_stream <<-SQL
-        SELECT u.userid, username, email, joindate, birthday, ipaddress, u.usergroupid, bandate, liftdate
+        SELECT u.userid, u.username, email, joindate, birthday, ipaddress, u.usergroupid, bandate, liftdate, mv.display_name, uf.field2
           FROM #{TABLE_PREFIX}user u
      LEFT JOIN #{TABLE_PREFIX}userban ub ON ub.userid = u.userid
+     LEFT JOIN #{TABLE_PREFIX}userfield uf ON uf.userid = u.userid
+     LEFT JOIN members_vbulletin2 mv ON mv.id = u.userid
          WHERE u.userid > #{@last_imported_user_id}
       ORDER BY u.userid
     SQL
@@ -108,6 +124,13 @@ class BulkImport::VBulletin < BulkImport::Base
         u[:suspended_at] = Time.zone.at(row[7])
         u[:suspended_till] = row[8] > 0 ? Time.zone.at(row[8]) : SUSPENDED_TILL
       end
+      if row[9].present?
+        u[:name] = normalize_text(row[9])
+      else
+        u[:name] = normalize_text(row[1])
+      end
+      u[:location] = normalize_text(row[10]) if row[10].present?
+
       u
     end
   end
@@ -298,11 +321,18 @@ class BulkImport::VBulletin < BulkImport::Base
 
     create_topics(topics) do |row|
       created_at = Time.zone.at(row[5])
+      if row[2].to_i == 13
+        category_id = SiteSetting.uncategorized_category_id
+      elsif row[2].to_i == 61
+        next
+      else
+        category_id = category_id_from_imported_id(row[2])
+      end
 
       t = {
         imported_id: row[0],
         title: normalize_text(row[1]),
-        category_id: category_id_from_imported_id(row[2]),
+        category_id: category_id,
         user_id: user_id_from_imported_id(row[3]),
         closed: row[4] == 0,
         created_at: created_at,
@@ -331,6 +361,7 @@ class BulkImport::VBulletin < BulkImport::Base
 
     create_posts(posts) do |row|
       topic_id = topic_id_from_imported_id(row[1])
+      next unless topic_id.present?
       replied_post_topic_id = topic_id_from_imported_post_id(row[2])
       reply_to_post_number = topic_id == replied_post_topic_id ? post_number_from_imported_id(row[2]) : nil
 
@@ -461,6 +492,127 @@ class BulkImport::VBulletin < BulkImport::Base
       }
     end
   end
+
+  def create_permalink_file
+    puts '', 'Creating Permalink File...', ''
+
+    id_mapping = []
+
+    Topic.listable_topics.find_each do |topic|
+      pcf = topic.first_post.custom_fields
+      if pcf && pcf["import_id"]
+        id = pcf["import_id"].split('-').last
+        id_mapping.push("XXX#{id}  YYY#{topic.id}")
+      end
+    end
+
+    # Category.find_each do |cat|
+    #   ccf = cat.custom_fields
+    #   if ccf && ccf["import_id"]
+    #     id = ccf["import_id"].to_i
+    #     id_mapping.push("/forumdisplay.php?#{id}  http://forum.quartertothree.com#{cat.url}")
+    #   end
+    # end
+
+    CSV.open(File.expand_path("../vb_map.csv", __FILE__), "w") do |csv|
+      id_mapping.each do |value|
+        csv << [value]
+      end
+    end
+  end
+
+
+  # find the uploaded file information from the db
+  def find_upload(post, attachment_id)
+    sql = "SELECT a.attachmentid attachment_id, a.userid user_id, a.filename filename,
+                  a.filedata filedata, a.extension extension
+             FROM #{TABLE_PREFIX}attachment a
+            WHERE a.attachmentid = #{attachment_id}"
+    results = mysql_query(sql)
+
+    unless row = results.first
+      puts "Couldn't find attachment record for attachment_id = #{attachment_id} post.id = #{post.id}"
+      return
+    end
+
+    attachment_id = row[0]
+    user_id = row[1]
+    db_filename = row[2]
+
+    filename = File.join(ATTACHMENT_DIR, user_id.to_s.split('').join('/'), "#{attachment_id}.attach")
+    real_filename = db_filename
+    real_filename.prepend SecureRandom.hex if real_filename[0] == '.'
+
+    unless File.exists?(filename)
+      puts "Attachment file #{row.inspect} doesn't exist"
+      return nil
+    end
+
+    upload = create_upload(post.user.id, filename, real_filename)
+
+    if upload.nil? || !upload.valid?
+      puts "Upload not valid :("
+      puts upload.errors.inspect if upload
+      return
+    end
+
+    [upload, real_filename]
+  rescue Mysql2::Error => e
+    puts "SQL Error"
+    puts e.message
+    puts sql
+  end
+
+
+  def import_attachments
+    puts '', 'importing attachments...'
+
+    RateLimiter.disable
+    current_count = 0
+
+    total_count = mysql_query(<<-SQL
+      SELECT COUNT(p.postid) count
+        FROM #{TABLE_PREFIX}post p
+        JOIN #{TABLE_PREFIX}thread t ON t.threadid = p.threadid
+       WHERE t.firstpostid <> p.postid
+    SQL
+    ).first[0].to_i
+
+    success_count = 0
+    fail_count = 0
+
+    attachment_regex = /\[attach[^\]]*\](\d+)\[\/attach\]/i
+
+    Post.find_each do |post|
+      current_count += 1
+      print_status current_count, total_count
+
+      new_raw = post.raw.dup
+      new_raw.gsub!(attachment_regex) do |s|
+        matches = attachment_regex.match(s)
+        attachment_id = matches[1]
+
+        upload, filename = find_upload(post, attachment_id)
+        unless upload
+          fail_count += 1
+          next
+          # should we strip invalid attach tags?
+        end
+
+        html_for_upload(upload, filename)
+      end
+
+      if new_raw != post.raw
+        PostRevisor.new(post).revise!(post.user, { raw: new_raw }, bypass_bump: true, edit_reason: 'Import attachments from vBulletin')
+      end
+
+      success_count += 1
+    end
+
+    puts "", "imported #{success_count} attachments... failed: #{fail_count}."
+    RateLimiter.enable
+  end
+
 
   def extract_pm_title(title)
     normalize_text(title).scrub.gsub(/^Re\s*:\s*/i, "")
