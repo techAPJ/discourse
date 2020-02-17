f1956e90860a464b20b73ed8c44e63c8446b3690
Arpit Jalan <arpit@techapj.com>
2020-02-17 19:15:56 +0530

    Jive JSON import for FreeRTOS.

--- script/import_scripts/jive_json.rb
+++ script/import_scripts/jive_json.rb
@@ -4,22 +4,29 @@ require_relative 'base.rb'
 require 'csv'
 
 class ImportScripts::JiveJson < ImportScripts::Base
-  DATA_FILE = '/home/ajalan/projects/freertos/data.csv'
+  JSON_FILE = '/home/ajalan/projects/freertos/data.json'
   USER_FILE = '/home/ajalan/projects/freertos/user_data.json'
 
   def initialize
     super
 
-    @system_user = Discourse.system_user
+    puts "loading post mappings..."
+    @post_number_map = {}
+    Post.pluck(:id, :post_number).each do |post_id, post_number|
+      @post_number_map[post_id] = post_number
+    end
+  end
+
+  def created_post(post)
+    @post_number_map[post.id] = post.post_number
+    super
   end
 
   def execute
     puts '', 'Importing from SourceForge...'
 
-    # load_json
-
-    import_users
     import_topics
+    mark_topics_as_solved
   end
 
   def csv_filename(table_name, use_fixed: true)
@@ -58,52 +65,31 @@ class ImportScripts::JiveJson < ImportScripts::Base
     arg
   end
 
-  def import_users
-    puts '', 'importing users'
-
-    users = JSON.parse(repair_json(File.read(USER_FILE)))
-    # {"userid"=>265482, "username"=>"zeni241", "name"=>"Zeni", "namevisible"=>"1", "email"=>"abc357@example.com",
-    # "usercreationdate"=>"2013-06-14 03:46:47", "usermodificationDate"=>"2016-06-15 11:44:39"}
-
-    create_users(users) do |user|
-      {
-        id: "jive-userid-#{user["userid"]}",
-        email: user["email"],
-        username: user["username"],
-        name: user["name"],
-        active: false,
-        created_at: parse_datetime(user["usercreationdate"])
-      }
-    end
-  end
-
   def import_topics
     puts '', 'importing posts'
 
-    # posts = JSON.parse(repair_json(File.read(JSON_FILE)))
-
-    posts = []
-    csv_parse("data") do |row|
-      posts << row.to_h
-    end
-
+    posts = JSON.parse(repair_json(File.read(JSON_FILE)))
     imported_post_count = 0
     total_post_count = posts.count
 
+    discobot_user_id = ::DiscourseNarrativeBot::Base.new.discobot_user.id
+
     create_posts(posts, total: total_post_count, offset: imported_post_count) do |post|
-      post_id = "jive-postid-#{post[:messageid]}"
-      first_post_id = "jive-postid-#{post[:rootmessageid]}"
+      # puts post.inspect
+      # exit
+      post_id = "jive-postid-#{post['messageid']}"
+      first_post_id = "jive-postid-#{post['rootmessageid']}"
 
       mapped = {
         id: post_id,
-        user_id: user_id_from_imported_user_id("jive-userid-#{post[:userid]}"),
-        created_at: parse_datetime(post[:messagecreationdate]),
-        raw: normalize_raw!(post[:body])
+        user_id: Discourse::SYSTEM_USER_ID,
+        created_at: parse_datetime(post['messagecreationdate']),
+        raw: normalize_raw!(post)
       }
 
       if post_id == first_post_id
-        # mapped[:category] = 2
-        mapped[:title] = post[:subject][0...255]
+        mapped[:category] = 35
+        mapped[:title] = post['subject'][0...255]
       else
         imported_topic = @lookup.topic_lookup_from_imported_post_id(first_post_id)
 
@@ -114,6 +100,23 @@ class ImportScripts::JiveJson < ImportScripts::Base
         end
 
         mapped[:topic_id] = imported_topic[:topic_id]
+        mapped[:custom_fields] = { is_accepted_answer: true, is_accepted_answer_from_import: true } if post['correctanswer'] == 1
+
+        reply_to_post_id = post_id_from_imported_post_id(post[:parentmessageid])
+        if reply_to_post_id
+          reply_to_post_number = @post_number_map[reply_to_post_id]
+          if reply_to_post_number && reply_to_post_number > 1
+            mapped[:reply_to_post_number] = reply_to_post_number
+          end
+        end
+
+        mapped[:post_create_action] = proc do |p|
+          if post['helpfulanswer'] == 1
+            user = User.new
+            user.id = discobot_user_id
+            PostActionCreator.like(user, p)
+          end
+        end
       end
 
       # puts mapped.inspect
@@ -125,13 +128,30 @@ class ImportScripts::JiveJson < ImportScripts::Base
 
   end
 
-  def normalize_raw!(raw)
+  def normalize_raw!(post)
+    # puts post.inspect
+    raw = post["body"]
     return "<missing>" if raw.blank?
     raw = raw.dup
 
     # new line
     raw.gsub!(/\\n/, "\n")
 
+    # code block
+    raw.gsub!(/\{code\}/, "\n```\n")
+
+    # fix quotes
+    raw.gsub!(/(^>.+\n)(?!^>)/, "#{$1}\n")
+    raw.gsub!(/> {quote:title=(.+?) wrote:},{quote}/i) { "> **#{$1}** wrote:" }
+
+    username = post['username']
+    user_link = "[#{username}](https://forums.aws.amazon.com/profile.jspa?userID=#{post['userid']})"
+    post_date = Time.zone.parse(post['messagecreationdate']).strftime('%B %d, %Y')
+
+    raw = "**#{user_link}** wrote on #{post_date}:\n\n#{raw}"
+
+    # puts raw
+    # exit
     # raw = CGI.unescapeHTML(raw)
     # raw = ReverseMarkdown.convert(raw)
     raw
@@ -140,6 +160,18 @@ class ImportScripts::JiveJson < ImportScripts::Base
   def parse_datetime(text)
     return nil if text.blank? || text == "null"
     DateTime.parse(text)
+  end
+
+  def mark_topics_as_solved
+    puts "", "Marking topics as solved..."
+
+    DB.exec <<~SQL
+      INSERT INTO topic_custom_fields (name, value, topic_id, created_at, updated_at)
+      SELECT 'accepted_answer_post_id', pcf.post_id, p.topic_id, p.created_at, p.created_at
+        FROM post_custom_fields pcf
+        JOIN posts p ON p.id = pcf.post_id
+       WHERE pcf.name = 'is_accepted_answer_from_import'
+    SQL
   end
 end
 
@@ -155,8 +187,8 @@ ImportScripts::JiveJson.new.perform
 
 # jq '. |= sort_by(.messageid)' export.json > data.json
 
-# jq 'map(del(.threadid, .rootmessageid, .messageid, .parentmessageid, .subject, .body, .helpfulanswer, .correctanswer, .threadcreationdate, .threadmodificationDate, .messagecreationdate, .messagemodificationDate))' user_raw.json > users.json
-# jq 'unique_by(.userid)' users.json > user_data.json
+# jq 'map(del(.threadid, .rootmessageid, .messageid, .parentmessageid, .subject, .body, .helpfulanswer, .correctanswer, .threadcreationdate, .threadmodificationDate, .messagecreationdate, .messagemodificationDate))' data.json > users.json
+# jq 'unique_by(.username)' users.json > user_data.json
 
 
 # CSV mans
